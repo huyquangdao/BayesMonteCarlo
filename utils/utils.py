152ce656898fc1185661d7af6b9223df2fa41624
trietdang5599 <trietdang05051999@gmail.com>
2025-11-20 17:59:06 +0800

    rm

--- utils/utils.py
+++ utils/utils.py
@@ -162,10 +162,10 @@ from baselines.GDP_Zero.pipeline import GDPZeroPipelineForNegotiation, GDPZeroPi
 # from pro_llm.pipeline import ProLLMPipelineForRecommendation, ProLLMPipelineForNegotiation, ProLLMPipelineForEmotionalSupport   
 
 # prompt refiner model
-from prompt_refiner.model import PromptRefinerModel
-from prompt_refiner.pipeline import PromptRefinerPipeline, PromptRefinerPipelineForNegotiation, PromptRefinerPipelineForEmotionalSupport, PromptRefinerPipelineForPersuation, PromptRefinerPipelineForRecommendation
-from prompt_refiner.trainer import PromptRefinerTrainer
-from prompt_refiner.config import PromptRefinerConfig, PromptRefinerConfigForNegotiation, PromptRefinerConfigForEmotionalSupport, PromptRefinerConfigForPersuation, PromptRefinerConfigForRecommendation
+# from prompt_refiner.model import PromptRefinerModel
+# from prompt_refiner.pipeline import PromptRefinerPipeline, PromptRefinerPipelineForNegotiation, PromptRefinerPipelineForEmotionalSupport, PromptRefinerPipelineForPersuation, PromptRefinerPipelineForRecommendation
+# from prompt_refiner.trainer import PromptRefinerTrainer
+# from prompt_refiner.config import PromptRefinerConfig, PromptRefinerConfigForNegotiation, PromptRefinerConfigForEmotionalSupport, PromptRefinerConfigForPersuation, PromptRefinerConfigForRecommendation
 
 # metrics for evaluation
 from eval.metric import Accuracy, PrecisionRecallF1, Item_Freq, SR, OfflineMetric, OnlineMetric, DistN, AverageTurn, \
