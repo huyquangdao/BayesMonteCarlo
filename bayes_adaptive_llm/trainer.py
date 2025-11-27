3b38f8c9eb5fdb391dd482da2d29571c4f4e8bb9
trietdang5599 <trietdang05051999@gmail.com>
2025-11-27 09:50:11 +0800

    fix

--- bayes_adaptive_llm/trainer.py
+++ bayes_adaptive_llm/trainer.py
@@ -31,6 +31,7 @@ from torch.utils.data import DataLoader
 from transformers import AutoModelForCausalLM, AutoTokenizer, get_linear_schedule_with_warmup
 from transformers.trainer_utils import IntervalStrategy
 from transformers.trainer import Trainer as HFTrainer
+from transformers.optimization import AdamW
 
 try:
     # Some TRL installs can raise RuntimeError if optional deps (e.g., openai) are missing.
