9c23e80285941558db7055c5a90f6951dd69c520
trietdang5599 <trietdang05051999@gmail.com>
2025-12-01 12:13:40 +0800

    fix heuristics

--- bayes_adaptive_llm/config.py
+++ bayes_adaptive_llm/config.py
@@ -107,9 +107,9 @@ class BayesAdaptiveConfigForEmotionalSupport(BayesAdaptiveConfig):
 
 class BayesAdaptiveConfigForPersuation(BayesAdaptiveConfig):
     # MCTS loop for preference pair generation
-    # mcts_num_evaluate = 1
-    num_mcts_sims = 30
-    max_realizations = 4
+    epsilon = 1.0
+    num_mcts_sims = 20
+    max_realizations = 3
     max_turns = 12
     combined_action = False
     special_tokens_dict = pg_special_tokens_dict
