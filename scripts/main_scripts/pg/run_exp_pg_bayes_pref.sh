2abfd80251d1ed106b00d8775e27cfa579926145
trietdang5599 <trietdang05051999@gmail.com>
2025-12-01 22:10:26 +0800

    fix

--- scripts/main_scripts/pg/run_exp_pg_bayes_pref.sh
+++ scripts/main_scripts/pg/run_exp_pg_bayes_pref.sh
@@ -7,7 +7,7 @@ EXPNAME="P4G_BAYES_PREF"
 
 for seed in 1
 do
-CUDA_VISIBLE_DEVICES=0,2,3,5 accelerate launch --main_process_port 8081 --gpu_ids 3 --num_processes 4 run.py \
+CUDA_VISIBLE_DEVICES=0,2,3,5 accelerate launch --main_process_port 8081 --gpu_ids 0,1,2,3 --num_processes 4 run.py \
   --exp_name "${EXPNAME}" \
   --project_name ProactiveLLM \
   --seed "${seed}" \
