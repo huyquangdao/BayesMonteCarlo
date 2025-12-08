5d4804dc51f701f5c3869f7d6412256efe1d32b7
trietdang5599 <trietdang05051999@gmail.com>
2025-12-08 09:29:23 +0800

    change model

--- scripts/main_scripts/pg/run_exp_pg_bayes_pref.sh
+++ scripts/main_scripts/pg/run_exp_pg_bayes_pref.sh
@@ -19,8 +19,8 @@ CUDA_VISIBLE_DEVICES=2 accelerate launch --main_process_port 8081 --gpu_ids 1 --
   --loggers terminal \
   --datasets p4g \
   --models bayes_adaptive_llm \
-  --gen_models qwen \
-  --model_type qwen \
+  --gen_models llama3 \
+  --model_type llama3 \
   --is_so_game \
   --use_persona \
   --rewrite_action \
