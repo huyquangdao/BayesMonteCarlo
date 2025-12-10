9920badaf11a3e1ba8dcb1fac27be940adb9fc3c
trietdang5599 <trietdang05051999@gmail.com>
2025-12-10 09:26:24 +0800

    config

--- scripts/main_scripts/pg/run_exp_pg_bayes_pref.sh
+++ scripts/main_scripts/pg/run_exp_pg_bayes_pref.sh
@@ -10,7 +10,7 @@ do
 NCCL_IB_DISABLE=1
 NCCL_P2P_DISABLE=1
 NCCL_ASYNC_ERROR_HANDLING=1
-CUDA_VISIBLE_DEVICES=1 accelerate launch --main_process_port 8081 --gpu_ids 1 --num_processes 1 run.py \
+CUDA_VISIBLE_DEVICES=2 accelerate launch --main_process_port 8081 --gpu_ids 1 --num_processes 1 run.py \
   --exp_name "${EXPNAME}" \
   --project_name ProactiveLLM \
   --seed "${seed}" \
