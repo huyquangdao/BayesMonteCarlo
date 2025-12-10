7b36364de7360965d41865c9c5da54f8dc7e7b9f
trietdang5599 <trietdang05051999@gmail.com>
2025-12-11 00:40:30 +0800

    fix

--- bayes_adaptive_llm/model.py
+++ bayes_adaptive_llm/model.py
@@ -35,8 +35,8 @@ class BayesAdaptiveLLMModel(Model):
         )
 
         # extend vocabulary with task-specific tokens
-        # self.tokenizer.add_special_tokens(self.model_config.special_tokens_dict)
-        # self.plm.resize_token_embeddings(len(self.tokenizer), mean_resizing=False)
+        self.tokenizer.add_special_tokens(self.model_config.special_tokens_dict)
+        self.plm.resize_token_embeddings(len(self.tokenizer), mean_resizing=False)
 
         if getattr(self.tokenizer, "chat_template", None) is None:
             self.tokenizer.chat_template = (
