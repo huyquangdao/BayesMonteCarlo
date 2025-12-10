2fefbdf4e5b3ad3d5c5f176ae40e543a3fa72795
trietdang5599 <trietdang05051999@gmail.com>
2025-12-11 00:31:03 +0800

    truncate

--- bayes_adaptive_llm/trainer.py
+++ bayes_adaptive_llm/trainer.py
@@ -769,7 +769,7 @@ class BayesAdaptiveLLMTrainer(Trainer):
         )
         input_prompt = train_dataset[0]['text']
         print("Input prompt for generation:", input_prompt)
-        response = self.model.generate_text(input_prompt)
+        response = self.model.generate_text(input_prompt, max_length=50)
         assert response is not None
         print("Generated response:", response)
         return response
