9452b0dd5a4ef80cef1fbdef726f02adbd29f394
trietdang5599 <trietdang05051999@gmail.com>
2025-12-11 20:34:11 +0800

    fix

--- bayes_adaptive_llm/model.py
+++ bayes_adaptive_llm/model.py
@@ -30,7 +30,8 @@ class BayesAdaptiveLLMModel(Model):
             self.model_config.plm,
             cache_dir=self.model_config.cached_dir,
             torch_dtype=dtype,
-            device_map="auto" if torch.cuda.is_available() else None, 
+            # device_map="auto" if torch.cuda.is_available() else None, 
+            device_map="cuda" if torch.cuda.is_available() else None,
         )
 
         # extend vocabulary with task-specific tokens
