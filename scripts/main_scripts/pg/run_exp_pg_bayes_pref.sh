802e8d6f2748ff64e0a1cc483bec805f218bb57e
trietdang5599 <trietdang05051999@gmail.com>
2025-12-12 19:08:16 +0800

    training

--- scripts/main_scripts/pg/run_exp_pg_bayes_pref.sh
+++ scripts/main_scripts/pg/run_exp_pg_bayes_pref.sh
@@ -10,7 +10,9 @@ do
 # NCCL_IB_DISABLE=1
 # NCCL_P2P_DISABLE=1
 # NCCL_ASYNC_ERROR_HANDLING=1
-CUDA_VISIBLE_DEVICES=3 accelerate launch --main_process_port 8081 --gpu_ids 3 --num_processes 1 run.py \
+
+#if train phase is needed, add --is_train
+CUDA_VISIBLE_DEVICES=2,3,4 accelerate launch --main_process_port 8081 --gpu_ids 2,3,4 --num_processes 3 run.py \
   --exp_name "${EXPNAME}" \
   --project_name ProactiveLLM \
   --seed "${seed}" \
@@ -21,6 +23,7 @@ CUDA_VISIBLE_DEVICES=3 accelerate launch --main_process_port 8081 --gpu_ids 3 --
   --models bayes_adaptive_llm \
   --gen_models llama3 \
   --model_type llama3 \
+  --is_train \
   --is_so_game \
   --use_persona \
   --is_utterance_based_action \
