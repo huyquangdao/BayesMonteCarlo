84a6407d037301aed213c046cb8f93e395313947
trietdang5599 <trietdang05051999@gmail.com>
2025-12-12 16:20:14 +0800

    config

--- scripts/main_scripts/pg/run_exp_pg_bayes_pref.sh
+++ scripts/main_scripts/pg/run_exp_pg_bayes_pref.sh
@@ -20,7 +20,7 @@ CUDA_VISIBLE_DEVICES=2,3,5 accelerate launch --main_process_port 8081 --gpu_ids 
   --datasets p4g \
   --models bayes_adaptive_llm \
   --gen_models llama3 \
-  --model_type llama3 \
+  --model_type train \
   --is_so_game \
   --use_persona \
   --is_utterance_based_action \
