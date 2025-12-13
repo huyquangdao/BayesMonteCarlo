78dcfe4afa7dc95f386741345f27fc64b4fb54ed
trietdang5599 <trietdang05051999@gmail.com>
2025-12-13 21:19:41 +0800

    test dpo

--- scripts/main_scripts/pg/run_exp_pg_bayes_pref.sh
+++ scripts/main_scripts/pg/run_exp_pg_bayes_pref.sh
@@ -22,7 +22,7 @@ CUDA_VISIBLE_DEVICES=4 accelerate launch --main_process_port 8081 --gpu_ids 4 --
   --datasets p4g \
   --models bayes_adaptive_llm \
   --gen_models llama3 \
-  --model_type train \
+  --model_type llama3 \
   --is_so_game \
   --use_persona \
   --num_train_rl_epochs 10 \
