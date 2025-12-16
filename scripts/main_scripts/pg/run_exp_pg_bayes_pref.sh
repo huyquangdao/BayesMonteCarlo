159ac459df87c8e494f38f8bc5a955397c87cdea
trietdang5599 <trietdang05051999@gmail.com>
2025-12-16 15:34:28 +0800

    test online 2k4

--- scripts/main_scripts/pg/run_exp_pg_bayes_pref.sh
+++ scripts/main_scripts/pg/run_exp_pg_bayes_pref.sh
@@ -12,7 +12,7 @@ do
 # NCCL_ASYNC_ERROR_HANDLING=1
 
 #if analysis_bayes_monte_carlo phase is needed, add --analysis_bayes_monte_carlo
-CUDA_VISIBLE_DEVICES=7 accelerate launch --main_process_port 8081 --gpu_ids 7 --num_processes 1 run.py \
+CUDA_VISIBLE_DEVICES=6 accelerate launch --main_process_port 8081 --gpu_ids 6 --num_processes 1 run.py \
   --exp_name "${EXPNAME}" \
   --project_name ProactiveLLM \
   --seed "${seed}" \
