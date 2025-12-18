d8944e2348bd8546ee35fd21653ac3658d9c3766
trietdang5599 <trietdang05051999@gmail.com>
2025-12-18 10:06:39 +0800

    eval sft p4g

--- scripts/main_scripts/pg/run_exp_pg_bayes_pref.sh
+++ scripts/main_scripts/pg/run_exp_pg_bayes_pref.sh
@@ -25,6 +25,7 @@ CUDA_VISIBLE_DEVICES=4 accelerate launch --main_process_port 8081 --gpu_ids 4 --
   --gen_models llama3 \
   --model_type llama3 \
   --is_so_game \
+  --is_utterance_based_action \
   --use_persona \
   --num_train_rl_epochs 10 \
   --metrics acc,prf1,sr,total_reward,avg_turn
