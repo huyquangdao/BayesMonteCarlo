249524b25b3a53298aebe81a995c3170019bfeaf
trietdang5599 <trietdang05051999@gmail.com>
2025-12-18 20:16:42 +0800

    collect gpu4

--- scripts/main_scripts/neg/run_exp_neg_bayes_pref.sh
+++ scripts/main_scripts/neg/run_exp_neg_bayes_pref.sh
@@ -13,7 +13,7 @@ do
 
 #if analysis_bayes_monte_carlo phase is needed, add --analysis_bayes_monte_carlo
 # ONLY add --is_utterance_based_action at evaluation stage
-CUDA_VISIBLE_DEVICES=7 accelerate launch --main_process_port 8081 --gpu_ids 7 --num_processes 1 run.py \
+  accelerate launch --main_process_port 8081 --gpu_ids 4 --num_processes 1 run.py \
   --exp_name "${EXPNAME}" \
   --project_name ProactiveLLM \
   --seed "${seed}" \
@@ -25,7 +25,6 @@ CUDA_VISIBLE_DEVICES=7 accelerate launch --main_process_port 8081 --gpu_ids 7 --
   --gen_models llama3 \
   --model_type llama3 \
   --is_so_game \
-  --is_utterance_based_action \
   --use_persona \
   --num_train_rl_epochs 10 \
   --metrics acc,prf1,sr,sl_ratio,avg_turn
