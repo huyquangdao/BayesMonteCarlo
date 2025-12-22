8703f9974007fd3f128b435b782273e0fca6d593
trietdang5599 <trietdang05051999@gmail.com>
2025-12-22 22:01:41 +0800

    eval llama3 extraversion

--- scripts/main_scripts/neg/run_exp_neg_bayes_pref.sh
+++ scripts/main_scripts/neg/run_exp_neg_bayes_pref.sh
@@ -13,7 +13,7 @@ do
 
 #if analysis_bayes_monte_carlo phase is needed, add --analysis_bayes_monte_carlo
 # ONLY add --is_utterance_based_action at evaluation stage
-  accelerate launch --main_process_port 8081 --gpu_ids 7 --num_processes 1 run.py \
+  accelerate launch --main_process_port 8081 --gpu_ids 5 --num_processes 1 run.py \
   --exp_name "${EXPNAME}" \
   --project_name ProactiveLLM \
   --seed "${seed}" \
@@ -23,7 +23,7 @@ do
   --datasets craigslist_bargain \
   --models bayes_adaptive_llm \
   --gen_models llama3 \
-  --model_type chatgpt \
+  --model_type llama3 \
   --is_so_game \
   --is_utterance_based_action \
   --use_persona \
