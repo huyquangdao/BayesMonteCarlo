a3323a4c92f22cc4d9112fde99a57bdb5a2eebf5
trietdang5599 <trietdang05051999@gmail.com>
2025-12-22 16:12:00 +0800

    eval base

--- scripts/main_scripts/neg/run_exp_neg_bayes_pref.sh
+++ scripts/main_scripts/neg/run_exp_neg_bayes_pref.sh
@@ -13,7 +13,7 @@ do
 
 #if analysis_bayes_monte_carlo phase is needed, add --analysis_bayes_monte_carlo
 # ONLY add --is_utterance_based_action at evaluation stage
-  accelerate launch --main_process_port 8081 --gpu_ids 4,5,6 --num_processes 3 run.py \
+  accelerate launch --main_process_port 8081 --gpu_ids 4 --num_processes 1 run.py \
   --exp_name "${EXPNAME}" \
   --project_name ProactiveLLM \
   --seed "${seed}" \
@@ -23,8 +23,9 @@ do
   --datasets craigslist_bargain \
   --models bayes_adaptive_llm \
   --gen_models llama3 \
-  --model_type train \
+  --model_type llama3 \
   --is_so_game \
+  --is_utterance_based_action \
   --use_persona \
   --num_train_rl_epochs 10 \
   --metrics acc,prf1,sr,sl_ratio,avg_turn
