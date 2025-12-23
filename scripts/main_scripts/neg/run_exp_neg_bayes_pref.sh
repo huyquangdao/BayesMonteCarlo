a863e755d8b11cb1759aff1b88641b0fc53cffa0
trietdang5599 <trietdang05051999@gmail.com>
2025-12-23 21:19:58 +0800

    sft

--- scripts/main_scripts/neg/run_exp_neg_bayes_pref.sh
+++ scripts/main_scripts/neg/run_exp_neg_bayes_pref.sh
@@ -5,7 +5,7 @@
 
 EXPNAME="NEG_BAYES_PREF"
 
-for seed in 42
+for seed in 1
 do
 # NCCL_IB_DISABLE=1
 # NCCL_P2P_DISABLE=1
@@ -13,7 +13,7 @@ do
 
 #if analysis_bayes_monte_carlo phase is needed, add --analysis_bayes_monte_carlo
 # ONLY add --is_utterance_based_action at evaluation stage
-  accelerate launch --main_process_port 8081 --gpu_ids 7 --num_processes 1 run.py \
+  accelerate launch --main_process_port 8081 --gpu_ids 3 --num_processes 1 run.py \
   --exp_name "${EXPNAME}" \
   --project_name ProactiveLLM \
   --seed "${seed}" \
