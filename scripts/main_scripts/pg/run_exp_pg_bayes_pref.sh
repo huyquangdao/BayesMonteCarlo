e3fd7d30839ae0a369e3ccbd1ca072c0c27b9f34
trietdang5599 <trietdang05051999@gmail.com>
2025-12-27 14:07:33 +0800

    eval base model

--- scripts/main_scripts/pg/run_exp_pg_bayes_pref.sh
+++ scripts/main_scripts/pg/run_exp_pg_bayes_pref.sh
@@ -14,7 +14,7 @@ do
 #if analysis_bayes_monte_carlo phase is needed, add --analysis_bayes_monte_carlo
 # ONLY add --is_utterance_based_action at evaluation stage
 # CUDA_VISIBLE_DEVICES=7 
-  accelerate launch --main_process_port 8081 --gpu_ids 5 --num_processes 1 run.py \
+  accelerate launch --main_process_port 8081 --gpu_ids 7 --num_processes 1 run.py \
   --exp_name "${EXPNAME}" \
   --project_name ProactiveLLM \
   --seed "${seed}" \
@@ -26,6 +26,7 @@ do
   --gen_models llama3 \
   --model_type llama3 \
   --is_so_game \
+  --use_persona \
   --is_utterance_based_action \
   --num_train_rl_epochs 10 \
   --metrics acc,prf1,sr,total_reward,avg_turn
