7349bd1f4c6bbeba0c09289d0b1a5f144f3c184b
trietdang5599 <trietdang05051999@gmail.com>
2026-01-05 14:31:13 +0700

    eval dpo nopersona

--- scripts/main_scripts/pg/run_exp_pg_bayes_pref.sh
+++ scripts/main_scripts/pg/run_exp_pg_bayes_pref.sh
@@ -14,7 +14,7 @@ do
 #if analysis_bayes_monte_carlo phase is needed, add --analysis_bayes_monte_carlo
 # ONLY add --is_utterance_based_action at evaluation stage
 # CUDA_VISIBLE_DEVICES=7 
-  accelerate launch --main_process_port 8081 --gpu_ids 5 --num_processes 1 run.py \
+  accelerate launch --main_process_port 8081 --gpu_ids 7 --num_processes 1 run.py \
   --exp_name "${EXPNAME}" \
   --project_name ProactiveLLM \
   --seed "${seed}" \
@@ -28,7 +28,6 @@ do
   --is_so_game \
   --use_persona \
   --is_utterance_based_action \
-  --analysis_bayes_monte_carlo \
   --num_train_rl_epochs 10 \
   --metrics acc,prf1,sr,total_reward,avg_turn
 
