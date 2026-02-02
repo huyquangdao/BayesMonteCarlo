91b543f68912e538673d9bc937fb9c1d38e55c07
trietdang5599 <trietdang05051999@gmail.com>
2026-02-02 11:20:14 +0800

    eval Conscientiousness

--- scripts/main_scripts/pg/run_exp_pg_proactive.sh
+++ scripts/main_scripts/pg/run_exp_pg_proactive.sh
@@ -1,7 +1,7 @@
 EXPNAME="Main"
 for i in 1
 do
-CUDA_VISIBLE_DEVICES=2 accelerate launch --main_process_port 71 --gpu_ids 2 --num_processes 1 run.py  \
+CUDA_VISIBLE_DEVICES=2 accelerate launch --main_process_port 71 --gpu_ids 3 --num_processes 1 run.py  \
         --exp_name $EXPNAME \
         --project_name ProactiveLLM \
         --seed $i \
@@ -11,8 +11,8 @@ CUDA_VISIBLE_DEVICES=2 accelerate launch --main_process_port 71 --gpu_ids 2 --nu
         --datasets p4g \
         --models proactive \
         --gen_models llama3 \
-        --is_so_game \
         --model_type llama3 \
+        --is_so_game \
         --use_persona \
         --metrics acc,prf1,sr,total_reward,avg_turn,bleu_n,rouge_n,dist_n        
 done
