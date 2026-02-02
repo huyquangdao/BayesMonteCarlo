5fba5f5b126acd3b8be4f1b0497b4aa11066da4f
trietdang5599 <trietdang05051999@gmail.com>
2026-02-02 11:03:54 +0800

    eval openness proactive

--- scripts/main_scripts/pg/run_exp_pg_proactive.sh
+++ scripts/main_scripts/pg/run_exp_pg_proactive.sh
@@ -13,6 +13,7 @@ CUDA_VISIBLE_DEVICES=1 accelerate launch --main_process_port 71 --gpu_ids 1 --nu
         --gen_models llama3 \
         --is_so_game \
         --model_type llama3 \
+        --is_utterance_based_action \
         --use_persona \
         --metrics acc,prf1,sr,total_reward,avg_turn,bleu_n,rouge_n,dist_n        
 done
