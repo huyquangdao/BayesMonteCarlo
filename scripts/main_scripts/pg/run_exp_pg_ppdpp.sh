1d260e88508734d0903ba799d21c191a382c2f1c
trietdang5599 <trietdang05051999@gmail.com>
2026-02-02 12:47:23 +0800

    fix

--- scripts/main_scripts/pg/run_exp_pg_ppdpp.sh
+++ scripts/main_scripts/pg/run_exp_pg_ppdpp.sh
@@ -15,6 +15,5 @@ CUDA_VISIBLE_DEVICES=2 accelerate launch --main_process_port 72 --gpu_ids 1 --nu
         --use_persona \
         --num_train_rl_epochs 5 \
         --model_type llama3 \
-        --analysis_bayes_monte_carlo \
         --metrics acc,prf1,sr,total_reward,avg_turn
 done
