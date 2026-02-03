c14f9fb9ddb2615e4e1a8ac269501ed3936d9d75
trietdang5599 <trietdang05051999@gmail.com>
2026-02-03 17:16:51 +0800

    train padpp

--- scripts/main_scripts/pg/run_exp_pg_so_padpp.sh
+++ scripts/main_scripts/pg/run_exp_pg_so_padpp.sh
@@ -1,7 +1,7 @@
 EXPNAME="Main"
-for i in 1
+for i in 42
 do
-CUDA_VISIBLE_DEVICES=4 accelerate launch --main_process_port 69 --gpu_ids 5 --num_processes 1 run.py  \
+CUDA_VISIBLE_DEVICES=4 accelerate launch --main_process_port 69 --gpu_ids 4 --num_processes 1 run.py  \
         --exp_name $EXPNAME \
         --project_name ProactiveLLM \
         --seed $i \
@@ -15,6 +15,5 @@ CUDA_VISIBLE_DEVICES=4 accelerate launch --main_process_port 69 --gpu_ids 5 --nu
         --is_so_game \
         --use_persona \
         --model_type llama3 \
-        --analysis_bayes_monte_carlo \
         --metrics acc,prf1,sr,total_reward,avg_turn
 done
