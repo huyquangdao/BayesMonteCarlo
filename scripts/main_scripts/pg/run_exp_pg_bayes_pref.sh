3c264718eb16340e34097f0c6d8589f2579cc8da
trietdang5599 <trietdang05051999@gmail.com>
2026-02-03 23:59:58 +0800

    eval conscienc

--- scripts/main_scripts/pg/run_exp_pg_bayes_pref.sh
+++ scripts/main_scripts/pg/run_exp_pg_bayes_pref.sh
@@ -14,7 +14,7 @@ do
 #if analysis_bayes_monte_carlo phase is needed, add --analysis_bayes_monte_carlo
 # ONLY add --is_utterance_based_action at evaluation stage
 # CUDA_VISIBLE_DEVICES=7 
-  accelerate launch --main_process_port 8081 --gpu_ids 5 --num_processes 1 run.py \
+  accelerate launch --main_process_port 8081 --gpu_ids 2 --num_processes 1 run.py \
   --exp_name "${EXPNAME}" \
   --project_name ProactiveLLM \
   --seed "${seed}" \
